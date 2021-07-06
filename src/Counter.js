3e791874a552d9c48ce71d917fc76aa2bef4a605
Yuvaraj <15032282+yuvgeek@users.noreply.github.com>
2021-07-06 18:58:18 +0530

    add the comments to counter component

--- src/Counter.js
+++ src/Counter.js
@@ -4,20 +4,26 @@ import "./Counter.css";
 export class Counter extends React.Component {
   constructor(props) {
     super(props);
+     // local state
     this.state = { count: 0, isStarted: false };
+    // This binding is necessary to make `this` work in the callback. eg (toggleButton)
     this.toggleButton = this.toggleButton.bind(this);
   }
 
   toggleButton() {
     if (!this.state.isStarted) {
+       // clicked Start button, so start the timer
       this.counterInterval = setInterval(() => {
-        this.setState((state, props) => ({
+        // Update the counter state
+        this.setState((state) => ({
           count: state.count + 1,
         }));
       }, 1000);
     } else {
+       // clicked stopped button, so clear the timer
       clearInterval(this.counterInterval);
     }
+    // update the isStarted state
     this.setState({
       isStarted: !this.state.isStarted,
     });
